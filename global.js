68190785be5bb5cdbabc7d2f86eb02f8ac061443
tanishaiyer1 <tiyer@ucsd.edu>
2025-10-16 16:04:43 -0700

    update

--- global.js
+++ global.js
@@ -48,19 +48,6 @@ for (let p of pages) {
   nav.append(a);
 }
 
-// document.body.insertAdjacentHTML(
-//   'afterbegin',
-//   `
-// 	<label class="color-scheme">
-// 		Theme:
-// 		<select>
-// 			<option value="light dark">Automatic</option>
-//             <option value="light">Light</option>
-//             <option value="dark">Dark</option>
-// 		</select>
-// 	</label>`,
-// );
-
 document.body.insertAdjacentHTML(
   'afterbegin',
   `
@@ -75,9 +62,16 @@ document.body.insertAdjacentHTML(
   `
 );
 
-
 const select = document.querySelector('.color-scheme select');
 select.addEventListener('input', function (event) {
-  console.log('color scheme changed to', event.target.value);
-  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  setColorScheme(event.target.value);
+  localStorage.colorScheme = event.target.value;
 });
+
+function setColorScheme(colorScheme) {
+  document.documentElement.style.setProperty('color-scheme', colorScheme);
+  select.value = colorScheme;
+}
+
+
+
