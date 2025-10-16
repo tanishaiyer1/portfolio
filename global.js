ab2df395476c6548b0e72b8f8676fa89e00d79f5
tanishaiyer1 <tiyer@ucsd.edu>
2025-10-16 15:53:00 -0700

    switcher code

--- global.js
+++ global.js
@@ -60,3 +60,9 @@ document.body.insertAdjacentHTML(
 		</select>
 	</label>`,
 );
+
+const select = document.querySelector('.color-scheme select');
+select.addEventListener('input', function (event) {
+  console.log('color scheme changed to', event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+});
