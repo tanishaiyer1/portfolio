9ed6bd939dd18657b024ca74d9de152677bf1f72
tanishaiyer1 <tiyer@ucsd.edu>
2025-10-16 15:58:23 -0700

    switcher update

--- global.js
+++ global.js
@@ -48,18 +48,33 @@ for (let p of pages) {
   nav.append(a);
 }
 
+// document.body.insertAdjacentHTML(
+//   'afterbegin',
+//   `
+// 	<label class="color-scheme">
+// 		Theme:
+// 		<select>
+// 			<option value="light dark">Automatic</option>
+//             <option value="light">Light</option>
+//             <option value="dark">Dark</option>
+// 		</select>
+// 	</label>`,
+// );
+
 document.body.insertAdjacentHTML(
   'afterbegin',
   `
-	<label class="color-scheme">
-		Theme:
-		<select>
-			<option value="light dark">Automatic</option>
+    <label class="color-scheme">
+        Theme:
+        <select>
+            <option value="light dark">Automatic</option>
             <option value="light">Light</option>
             <option value="dark">Dark</option>
-		</select>
-	</label>`,
+        </select>
+    </label>
+  `
 );
+
 
 const select = document.querySelector('.color-scheme select');
 select.addEventListener('input', function (event) {
