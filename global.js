298ecc6c5f36b5b38a4cfe820dc557937fe4dc64
tanishaiyer1 <tiyer@ucsd.edu>
2025-10-23 23:12:34 -0700

    projects file commit

--- global.js
+++ global.js
@@ -78,3 +78,22 @@ select.addEventListener('input', function (event) {
 if ('colorScheme' in localStorage) {
   setColorScheme(localStorage.colorScheme);
 }
+
+export async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+import { fetchJSON, renderProjects } from '../global.js';
+
+const projects = await fetchJSON('../lib/projects.json');
+const projectsContainer = document.querySelector('.projects');
+renderProjects(projects, projectsContainer, 'h2');
