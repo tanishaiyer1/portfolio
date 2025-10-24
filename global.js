d6d4ba3c7356e59a5288b1651ab4435196075231
tanishaiyer1 <tiyer@ucsd.edu>
2025-10-24 00:31:27 -0700

    index updates

--- global.js
+++ global.js
@@ -4,13 +4,6 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// let navLinks = $$("nav a");
-// let currentLink = navLinks.find(
-//   (a) => a.host === location.host && a.pathname === location.pathname,
-// );
-// currentLink?.classList.add('current');
-
-
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                     
   : "/portfolio/";
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
+import { fetchJSON, renderProjects } from './global.js';
+
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
-
+renderProjects(latestProjects, projectsContainer, 'h2');
