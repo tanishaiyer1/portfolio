0f3789e500c857e498776e9f787b2813a0313fc9
tanishaiyer1 <tiyer@ucsd.edu>
2025-10-30 10:26:30 -0700

    step 1

--- projects/projects.js
+++ projects/projects.js
@@ -3,3 +3,17 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
+
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+let svg = d3.select("#projects-pie-plot");
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
