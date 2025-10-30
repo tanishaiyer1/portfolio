0295a6d0ce7d72b8e202762d3f822f7459a5848e
tanishaiyer1 <tiyer@ucsd.edu>
2025-10-30 11:05:23 -0700

    Pie chart, step 2

--- projects/projects.js
+++ projects/projects.js
@@ -10,10 +10,18 @@ let svg = d3.select("#projects-pie-plot");
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 
 let arcs = arcData.map((d) => arcGenerator(d));
@@ -21,5 +29,15 @@ let arcs = arcData.map((d) => arcGenerator(d));
 arcs.forEach((arc, idx) => {
   svg.append('path')
     .attr('d', arc)
-    .attr('fill', colors[idx]);
+    .attr('fill', colors(idx));
+});
+
+let legend = d3.select('.legend');
+
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`)
+    .attr('class', 'legend-item')
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
