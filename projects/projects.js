e28e7d77c6333683c66fa2ad3703ba561c796c4d
tanishaiyer1 <tiyer@ucsd.edu>
2025-10-30 10:31:35 -0700

    pie charts?

--- projects/projects.js
+++ projects/projects.js
@@ -10,6 +10,32 @@ let svg = d3.select("#projects-pie-plot");
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
+let data = [1, 2];
+let colors = ['gold', 'purple'];
+
+let total = 0;
+for (let d of data) {
+  total += d;
+}
+
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr(...) => ({ fill: colors[idx] })
+})
+
 let arc = arcGenerator({
   startAngle: 0,
   endAngle: 2 * Math.PI,
