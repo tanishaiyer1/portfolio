6d1fe7a7233a992a1b6dba4655735948aeaa36c8
tanishaiyer1 <tiyer@ucsd.edu>
2025-10-30 10:35:49 -0700

    d3.pie

--- projects/projects.js
+++ projects/projects.js
@@ -13,33 +13,16 @@ let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 let data = [1, 2];
 let colors = ['gold', 'purple'];
 
-let total = 0;
-for (let d of data) {
-  total += d;
-}
-
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
 
 let arcs = arcData.map((d) => arcGenerator(d));
 
 arcs.forEach((arc, idx) => {
-    d3.select('svg')
-      .append('path')
-      .attr('d', arc)
-      .attr(...) => ({ fill: colors[idx] })
-})
-
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
+  svg.append('path')
+    .attr('d', arc)
+    .attr('fill', colors[idx]);
 });
 
-d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+// d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
